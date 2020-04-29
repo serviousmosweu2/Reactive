c5a73055f46edbdddf79b6a46f5c9137d4ef160f
Servious Dikotope <serviousmosweu2@gmail.com>
2020-04-29 11:55:44 +0200

    End Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,11 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>{
+opt.AddPolicy("CorsPolicy", policy=>{
+    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
+            });
             services.AddControllers();
             
             //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
@@ -48,6 +53,7 @@ namespace API
 
             //app.UseHttpsRedirection();
             //app.UseMvc();
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
